8eb6a3260277111aedc0d9ae04f1a18b903450f8
bhavyatrivedi01 <36411614+bhavyatrivedi01@users.noreply.github.com>
2023-09-20 21:34:40 -0600

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,8 +1,8 @@
-ssh ubuntu@34.207.74.36 <<EOF
+ssh ubuntu@54.81.4.126 <<EOF
   cd django
   git pull 
   source myprojectenv/bin/activate
   ./manage.py migrate
-  ./manage.py 34.207.74.36:8000
+  ./manage.py 54.81.4.126:8000
   exit
 EOF
