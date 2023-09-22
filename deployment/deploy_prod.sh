763d469924ce7090fabae1b879637bc89eb3dfe8
bhavyatrivedi01 <36411614+bhavyatrivedi01@users.noreply.github.com>
2023-09-22 15:00:10 -0600

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -3,7 +3,7 @@ ssh root@ec2-54-88-212-144.compute-1.amazonaws.com <<EOF
   git pull 
   source myprojectenv/bin/activate
   cd myprojectenv/Blood-Bank-management-Python
-  ./manage.py migrate
-  ./manage.py runserver 172.31.37.167:8000
+  python3 ./manage.py migrate
+  python3 ./manage.py runserver 172.31.37.167:8000
   exit
 EOF
