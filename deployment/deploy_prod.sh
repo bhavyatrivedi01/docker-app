c52264e9ae7b0a99972e6b8b2c8b99f7e162feed
bhavyatrivedi01 <36411614+bhavyatrivedi01@users.noreply.github.com>
2023-09-23 01:34:28 -0600

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,9 +1,2 @@
-ssh root@ec2-54-88-212-144.compute-1.amazonaws.com <<EOF
-  cd django
-  git pull 
-  source myprojectenv/bin/activate
-  cd myprojectenv/Blood-Bank-management-Python
-  python3 ./manage.py migrate
-  python3 ./manage.py runserver 172.31.37.167:8000
-  exit
+ssh root@ec2-54-88-212-144.compute-1.amazonaws.com
 EOF
