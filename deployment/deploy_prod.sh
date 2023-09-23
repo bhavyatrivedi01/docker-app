ab568fb4cd78aa42e2350c1a1867be204921f683
bhavyatrivedi01 <36411614+bhavyatrivedi01@users.noreply.github.com>
2023-09-23 01:49:30 -0600

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,2 +1,10 @@
-ssh root@ec2-54-88-212-144.compute-1.amazonaws.com
+ssh root@ec2-54-88-212-144.compute-1.amazonaws.com <<EOF
+  cd django/myprojectenv
+  git pull
+  cd ..
+  source myprojectenv/bin/activate
+  python3 ./manage.py makemigrations
+  python3 ./manage.py migrate  --run-syncdb
+  sudo service apache2 restart
+  exit
 EOF
