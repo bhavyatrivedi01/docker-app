97da17881e4b93329d74667db280bc03b8cb0735
bhavyatrivedi01 <36411614+bhavyatrivedi01@users.noreply.github.com>
2023-09-23 02:48:03 -0600

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,4 +1,8 @@
-ssh ubuntu@172.31.37.167 <<EOF
+ssh root@ec2-54-196-162-237.compute-1.amazonaws.com <<EOF
+  cd django
+  git pull
+  source myprojectenv/bin/activate
+  ./manage.py migrate
   sudo service apache2 restart
   exit
 EOF
