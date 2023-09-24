f767fba0eb4024c72d1528641d86add360d3a6b4
bhavyatrivedi01 <36411614+bhavyatrivedi01@users.noreply.github.com>
2023-09-24 00:21:08 -0600

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,8 +1,6 @@
   ssh ubuntu@34.207.134.222 <<EOF
-  cd django
+  cd django/myprojectenv/Blood-Bank-management-Python
   git pull
-  source myprojectenv/bin/activate
-  ./manage.py migrate
   sudo service apache2 restart
   exit
 EOF
