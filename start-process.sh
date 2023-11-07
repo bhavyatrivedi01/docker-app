d873ceafbd397814110cde27b92fa9f1c4a63e4a
bhavyatrivedi01 <36411614+bhavyatrivedi01@users.noreply.github.com>
2023-11-07 14:58:05 -0600

    Update start-process.sh

--- start-process.sh
+++ start-process.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
-docker tag $IMAGE_REPO_NAME:v2 $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:v2
-docker run -d -p 8000:8000 $IMAGE_REPO_NAME
+docker tag $IMAGE_REPO_NAME:v3 $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:v3
+#docker run -d -p 8000:8000 $IMAGE_REPO_NAME
 
