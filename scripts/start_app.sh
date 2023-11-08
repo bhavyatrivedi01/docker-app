08ac22caf6ebcc94c0ab8fbe7d77be517af523d7
bhavyatrivedi01 <36411614+bhavyatrivedi01@users.noreply.github.com>
2023-11-08 14:46:00 -0600

    changes

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash 
 
 aws ecr get-login-password --region region | docker login --username AWS --password-stdin 998503830099.dkr.ecr.us-east-1.amazonaws.com
-docker pull 998503830099.dkr.ecr.us-east-1.amazonaws.com/production:v3
-docker run -d -p 8000:8000 998503830099.dkr.ecr.us-east-1.amazonaws.com/production:v3
+docker pull 998503830099.dkr.ecr.us-east-1.amazonaws.com/production:v4
+docker run -d -p 8000:8000 998503830099.dkr.ecr.us-east-1.amazonaws.com/production:v4
