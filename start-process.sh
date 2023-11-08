98f34b5efb5ca2afa20751e55fe4343400f980ba
bhavyatrivedi01 <36411614+bhavyatrivedi01@users.noreply.github.com>
2023-11-08 13:44:43 -0600

    commit

--- start-process.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
-docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
-docker tag $IMAGE_REPO_NAME:v3 $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:v3
-#docker run -d -p 8000:8000 $IMAGE_REPO_NAME
-
